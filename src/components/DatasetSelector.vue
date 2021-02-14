cb0a4e3b8594afd913c3c28d2e08c3e2965affb3
Archit Rathore <architrathore1@gmail.com>
2021-02-14 00:52:04 -0700

    Make dataset selector reactive

--- src/components/DatasetSelector.vue
+++ src/components/DatasetSelector.vue
@@ -3,13 +3,15 @@
     <label for="dataset-selector" style="display:inline-block">Dataset:</label>
     <select id="dataset-selector" style="display: inline-block" class="form-select" v-model="selection" v-on:change="changeDataset(selection)">
 <!--      <option disabled value="" selected>Select a dataset</option>-->
-      <option value="euclidean_l2_50">Euclidean-L2-50intervals</option>
-      <option value="euclidean_l2_100">Euclidean-L2-100intervals</option>
+      <option value="euclidean_l2_50">Euclidean - L2 - 50intervals</option>
+      <option value="euclidean_l2_100">Euclidean - L2 - 100intervals</option>
     </select>
   </div>
 </template>
 
 <script>
+import {mapState} from "vuex";
+
 export default {
   name: "DatasetSelector",
   data() {
@@ -17,6 +19,9 @@ export default {
       selection: 'euclidean_l2_50'
     }
   },
+  computed: mapState({
+    selection: state => state.currentDataset,
+  }),
   methods: {
     changeDataset(newDataset) {
       console.log(newDataset)
