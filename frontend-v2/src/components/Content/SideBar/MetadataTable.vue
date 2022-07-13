3f7cab5bddd01c72014fc766669f17827ef30ece
Archit Rathore <architrathore1@gmail.com>
2022-07-13 15:30:02 -0600

    revert darkcolor

--- frontend-v2/src/components/Content/SideBar/MetadataTable.vue
+++ frontend-v2/src/components/Content/SideBar/MetadataTable.vue
@@ -28,6 +28,7 @@
     let g = parseInt(color.substring(2, 4), 16); // hexToG
     let b = parseInt(color.substring(4, 6), 16); // hexToB
     return r * 0.299 + g * 0.587 + b * 0.114 > 186 ? darkColor : lightColor;
+    // return darkColor;
   }
 
   function bgColor(classLabel: string) {
